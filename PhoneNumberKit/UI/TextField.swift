7ffdec7d5932b0d3a378128274547b72c47189cb
edgard <edgard.jammal@gotocme.com>
2020-02-19 14:57:23 +0200

    dismiss in all cases

--- PhoneNumberKit/UI/TextField.swift
+++ PhoneNumberKit/UI/TextField.swift
@@ -455,12 +455,7 @@ extension PhoneNumberTextField: CountryCodePickerDelegate {
         partialFormatter.defaultRegion = country.code
         updateFlag()
         updatePlaceholder()
-
-        if let nav = containingViewController?.navigationController {
-            nav.popViewController(animated: true)
-        } else {
-            containingViewController?.dismiss(animated: true)
-        }
+        containingViewController?.dismiss(animated: true)
     }
 }
 
